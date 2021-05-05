009956325266791a31367e995eeac6aa92f2b6b8
unknown <daniel.nagami@gmail.com>
2021-05-05 01:49:51 -0300

    Testing code reader

---
+++ src/RobotTR/CodeAnalyzerLab/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace CodeAnalyzerLab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var files = new List<string>();
+            GetCodeFiles("C:\\Studies\\desenvolvedor.io\\NerdStoreEnterprise", ref files);
+
+            int fileCounter = 1;
+            foreach (var file in files)
+            {
+                Console.WriteLine($"File number {fileCounter} | Directory: {file}");
+                Console.WriteLine("-----------------------------------------------");
+                ReadFile(file);
+                Console.WriteLine("-----------------------------------------------");
+                fileCounter++;
+            }
+        }
+
+        private static void GetCodeFiles(string rootDirectory, ref List<string> filesPaths)
+        {
+            var files = Directory.GetFiles(rootDirectory);
+
+            foreach (var file in files)
+            {
+                if (file.EndsWith(".cs"))
+                    filesPaths.Add(file);
+            }
+
+            var folders = Directory.GetDirectories(rootDirectory);
+
+            foreach (var folder in folders)
+            {
+                if(!folder.EndsWith("obj"))
+                    GetCodeFiles(folder, ref filesPaths);
+            }
+        }
+
+        private static void ReadFile(string filePath)
+        {
+            using (var sr = new StreamReader(filePath))
+            {
+                Console.WriteLine(sr.ReadToEnd());
+            }
+        }
+    }
+}
